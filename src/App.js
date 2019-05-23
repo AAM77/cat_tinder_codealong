fd6e5abdbdc472c688f9d16a7ab32ecf2b6466b6
github username <Adeel.A.Mhd@gmail.com>
2019-05-23 19:36:32 -0400

    Replaced the boilerplate with our own code (did all this before creating a repo.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Cats from './components/Cats';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log(React)
+    return (
+      <div className="App">
+        <h1>Welcome to Cat Tinder</h1>
+        <Cats />
+      </div>
+    );
+  }
 }
 
 export default App;

24fe5797be4c98f7d6f0fce85e4b30f757a4c2bb
github username <Adeel.A.Mhd@gmail.com>
2019-05-24 16:53:36 -0400

    Passed along the handleClick function (in the form of a handleChangeOfHeart prop) to the CatCard component.

--- src/components/LikedCats.js
+++ src/components/LikedCats.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import CatCard from './CatCard';
 
-const LikedCats = ({ cats }) => {
-  const catCards = cats.map(cat => <CatCard key={cat.id} cat={cat} />);
+const LikedCats = ({ cats, handleChangeOfHeart }) => {
+  const catCards = cats.map(cat => (
+    <CatCard
+      key={cat.id}
+      cat={cat}
+      handleChangeOfHeart={handleChangeOfHeart}
+    />
+  )
+);
 
   return (
     <div className="LikedCats">
       <h2>Liked Cats</h2>
-
       { catCards }
     </div>
   )

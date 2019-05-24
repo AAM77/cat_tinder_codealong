5e2356272cca3d479d5b6b076e5812c1efabef97
github username <Adeel.A.Mhd@gmail.com>
2019-05-23 22:27:03 -0400

    Moved the data import, state, and componentDidMount() into App.js. Passed the state's data as a prop down to Cats & DisplayCat. Had to handle the initial empty array before componentDidMount() reset the state so there wouldn't be an error. Used a ternary operator with a fragment to accomplish this.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import Cats from './components/Cats';
+import DisplayCat from './components/DisplayCat';
+
+import cats from './cats-data';
 import './App.css';
 
+
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      cats: []
+    }
+  }
+
+  // mocking a fetch request
+  componentDidMount() {
+    // fetch("www.mycats.com")
+    // .then( response => response.json())
+    // ,.then(cats => this.setState({cats: cats}) )
+    this.setState({
+      cats: cats
+    })
+  }
+
+
   render() {
-    console.log(React)
+
     return (
       <div className="App">
         <h1>Welcome to Cat Tinder</h1>
-        <Cats />
+        { this.state.cats[0] ? <> <DisplayCat cat={this.state.cats[0]} /> <Cats cats={this.state.cats} /> </> : "Gathering the cats together..." }
+
+
       </div>
     );
   }

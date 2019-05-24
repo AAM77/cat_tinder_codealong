c0f0f97ea3cc0e14a264385d92588192cfc63c1a
github username <Adeel.A.Mhd@gmail.com>
2019-05-23 23:06:15 -0400

    Added a class name

--- src/components/CatCard.js
+++ src/components/CatCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const CatCard = ({ cat }) => {
   return(
-    <div>
+    <div className="CatCard">
       <img height="100px" width="100px" src={cat.image_url} alt={cat.name} /><br />
       <span>{cat.name}</span>
     </div>

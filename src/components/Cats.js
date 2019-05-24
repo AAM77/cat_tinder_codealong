680d9522d1100c46f7a9ad41275754b3098bf6c9
github username <Adeel.A.Mhd@gmail.com>
2019-05-23 22:24:15 -0400

    Moved the data import, state, and componentDidMount() to App.js. Then, passed the data down through App to Cats as a prop.

--- src/components/Cats.js
+++ src/components/Cats.js
@@ -1,35 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import CatCard from './CatCard';
-import cats from '../cats-data';
 
 
-export default class Cats extends Component {
 
-  constructor() {
-    super()
-    this.state = {
-      cats: []
-    }
-  }
+const Cats = ({ cats }) => {
+  const catCards = cats.map( cat => <CatCard key={cat.name} cat={cat} />)
 
-  // mocking a fetch request
-  componentDidMount() {
-    // fetch("www.mycats.com")
-    // .then( response => response.json())
-    // ,.then(cats => this.setState({cats: cats}) )
-    this.setState({
-      cats: cats
-    })
-  }
-
-  render() {
-
-    const catCards = this.state.cats.map( cat => <CatCard key={cat.name} cat={cat} />)
-
-    return (
-      <div>
-        { catCards }
-      </div>
-    );
-  };
+  return (
+    <div>
+      { catCards }
+    </div>
+  );
 }
+
+export default Cats;

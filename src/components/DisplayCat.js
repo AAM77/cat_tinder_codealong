071b24465a471b729c089ad8823ae972f694bac6
github username <Adeel.A.Mhd@gmail.com>
2019-05-24 15:18:54 -0400

    Added an id attribute to the like and dislike buttons. Restructure the way style const was written.

--- src/components/DisplayCat.js
+++ src/components/DisplayCat.js
@@ -3,12 +3,17 @@ import CatCard from './CatCard';
 
 const DisplayCat = ({ cat, handleLikeClick, handleDislikeClick }) => {
 
-  const style = { borderStyle:"solid", borderColor:"blue", borderWidth:"1px"}
+  const style = {
+    borderStyle:"solid",
+    borderColor:"blue",
+    borderWidth:"1px"
+  }
+
   return (
     <div style={style} className="DisplayCat">
       <CatCard cat={cat} />
-      <button onClick={handleLikeClick}>Like</button>
-      <button onClick={handleDislikeClick}>Dislike</button>
+      <button id={ cat ? cat.id : "no-cat" } onClick={handleLikeClick}>Like</button>
+      <button id={ cat ? cat.id : "no-cat" } onClick={handleDislikeClick}>Dislike</button>
     </div>
   )
 }

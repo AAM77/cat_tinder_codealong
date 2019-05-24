b18de8f4a3acc5f53db5165d0a678fb1549d5fea
github username <Adeel.A.Mhd@gmail.com>
2019-05-23 23:36:28 -0400

    Deleted the width property from the in-line styling.

--- src/components/DisplayCat.js
+++ src/components/DisplayCat.js
@@ -3,7 +3,7 @@ import CatCard from './CatCard';
 
 const DisplayCat = ({ cat }) => {
 
-  const style = { borderStyle:"solid", borderColor:"blue", borderWidth:"1px", width:"150px"}
+  const style = { borderStyle:"solid", borderColor:"blue", borderWidth:"1px"}
   return (
     <div style={style} className="DisplayCat">
       <CatCard cat={cat} />

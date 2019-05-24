6a27d6281e39c6794b97d5d85f2d132d4947e7b3
github username <Adeel.A.Mhd@gmail.com>
2019-05-24 16:56:47 -0400

    Added an onClick listener to trigget handleChangeOfHeart (i.e. handleClick) upon clicking the image. It also passes down an id.

--- src/components/CatCard.js
+++ src/components/CatCard.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 
-const CatCard = ({ cat }) => {
+const CatCard = ({ cat, handleChangeOfHeart }) => {
 
   let returnVal;
 
   if (cat) {
     returnVal = <div className="CatCard">
-                  <img key={cat.id} src={cat.image_url} alt={cat.name} /><br />
+                  <img
+                    id={cat ? cat.id : "no-cat"}
+                    key={cat.id} src={cat.image_url}
+                    alt={cat.name}
+                    onClick={handleChangeOfHeart}
+                  />
+                  <br />
                   <span>{cat.name}</span>
+                  <br />
+                  <br />
                 </div>
   } else {
     returnVal = <div>

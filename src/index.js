1395d24b9e7097c9bd6157edfea5fabd72b101be
github username <Adeel.A.Mhd@gmail.com>
2019-05-26 18:31:25 -0400

    imported provider from react-redux as well as createStore & applyMiddleware from redux. Added/Linked the App component to the Provider. Added a mock database of three cats. Created a single action getCats. Created a single very basic reducer. Created a store using createStore and linked it to the REDUX_DEVTOOLS_EXTENSION.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,61 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//The redux libraries are: redux, react-redux, redux-thunk
+// the ingredients I need are"
+//    - reducers
+//    - store
+//    - store
+//    - actions (creators)
+
+const cats = [
+  {status: "undecided", image_url: "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", name: "Fluffy"},
+  {status: "undecided", image_url: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", name: "Peaches"},
+  {status: "liked", image_url: "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", name: "Spike"}
+]
+
+export const getCats = {
+  type: "GET_CATS",
+  cats: cats
+}
+
+// reducer
+const reducer = (state = { cats: [] }, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+};
+
+// store
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
+
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+// This (index.js) is an important file because
+// (1) It is what calls 'App' and we need to put 'App'
+//     in the 'Provider' (a part of redux)
+// (2) It is where createStore goes
+
+// first, though, we need to import 'Provider'

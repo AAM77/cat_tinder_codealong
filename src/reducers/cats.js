9882573ac29c982c95c0090e558076c986038df0
github username <Adeel.A.Mhd@gmail.com>
2019-05-27 15:35:40 -0400

    Added another case to handle updating the state when the action.type equals CHANGE_STATUS

--- src/reducers/cats.js
+++ src/reducers/cats.js
@@ -3,6 +3,8 @@ export default (cats = [], action) => {
   switch (action.type) {
     case "SET_CATS":
       return action.cats
+    case "CHANGE_STATUS":
+      return action.cats
     default:
       return cats
   }

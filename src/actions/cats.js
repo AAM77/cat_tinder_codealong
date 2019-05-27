3ee8721a89954a4b3189c1a435580dc88bb2d0b1
github username <Adeel.A.Mhd@gmail.com>
2019-05-27 13:25:17 -0400

    Added cats.js to the actions folder. Inside it: defined an action creator of setCats that creates and return a new state populated with the 'cats' passed to it as an argument. Also added an asynchronous action creator that fetches the cats data from the backend, passes it to the setCats action creator and dispatches it to props.

---
+++ src/actions/cats.js
@@ -0,0 +1,42 @@
+// action creators
+// an action creator is a function that returns an action
+// for example, an action creator would be:
+// export const setCats = cats => {
+//
+// }
+// This CANNOT all be on the same line !!!!!! The curly braces need to be
+// opened up into curly braces onto multiple lines with a return statement.
+
+// action create vs action: an action creator is simply a function
+// that returns an action object with a type (instructions) and payload (the data that we are interacting with)
+
+export const setCats = cats => {
+  return {type: 'SET_CATS', cats: cats}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// before we can do anything with our action creators, however, we
+// need to first GET those cats from a backend database
+// so that we can actually use the actions we built to
+// interact with the global state in redux.
+
+// this is where asyn action creators come into play.
+
+// async action creators
+export const getCats = () => {
+  return dispatch => {
+    return fetch("http://localhost:3001/cats")
+      .then( response => response.json())
+      .then( cats => (dispatch(setCats(cats))))
+  }
+}

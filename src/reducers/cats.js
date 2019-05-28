a962c2d952b43419109c5e2059a80f9135822209
github username <Adeel.A.Mhd@gmail.com>
2019-05-27 20:18:40 -0400

    Added in new case statements to handle the different actions. Used virutally the same code three times. This needs to change to reduce 'code-smell'. Also changes 'cats' to 'state'for the reducer's first argument.

--- src/reducers/cats.js
+++ src/reducers/cats.js
@@ -1,11 +1,54 @@
 // reducer
-export default (cats = [], action) => {
+export default (state = [], action) => {
+  let newCatsArr;
+
   switch (action.type) {
+
     case "SET_CATS":
       return action.cats
+
+    case "LIKE_CAT":
+       newCatsArr = state.map( cat => {
+        if (action.catId === cat.id) {
+          const newCat = Object.assign({}, cat);
+          newCat.status = 'liked';
+          return newCat
+        } else {
+          return cat
+        }
+      });
+      return newCatsArr;
+
+    case "DISLIKE_CAT":
+      newCatsArr = state.map( cat => {
+        if (action.catId === cat.id) {
+          const newCat = Object.assign({}, cat);
+          newCat.status = 'disliked'
+          return newCat
+        } else {
+          return cat
+        }
+      });
+
+      return newCatsArr;
+
+    case 'CHANGE_HEART':
+      newCatsArr = state.map( cat => {
+        if (action.catId === cat.id) {
+          const newCat = Object.assign({}, cat);
+          newCat.status = "undecided";
+          return newCat
+        } else {
+          return cat
+        }
+      });
+
+      return newCatsArr;
+
     case "CHANGE_STATUS":
       return action.cats
+
     default:
-      return cats
+      return state
   }
 };
